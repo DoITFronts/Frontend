5c203bf4a6fc9c83f006f783dd9acd4aa6144550
siggu <m0_w_0m@naver.com>
2025-03-02 19:12:27 +0900

    fix: meeting id 타입 변경
    - string -> id

--- api/meeting/toggleLike.ts
+++ api/meeting/toggleLike.ts
@@ -1,4 +1,4 @@
-const toggleLike = async (meetingId: string) => {
+const toggleLike = async (meetingId: number) => {
   // TODO: 실제 좋아요 동작 구현
   console.log(`좋아요 누름! - Meeting ID: ${meetingId}`);
   return { success: true };
