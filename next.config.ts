f15e7160c1f460a5b0bb209dfefb32ff16c95a0b
Choi Seung-eun <138289674+xeunnie@users.noreply.github.com>
2025-03-05 10:05:16 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(',') || [],
+    domains: process.env.NEXT_PUBLIC_API_BASE_URL?.split(',') || [],
     remotePatterns: [
       {
         protocol: 'https',
