5799fd17c258ac63cc3a48b68aae4c79027159b8
Oh_hong <jungi9933@naver.com>
2025-03-05 11:33:03 +0900

    feat: 프로필 수정 아이콘 추가

--- app/myprofile/@profile/page.tsx
+++ app/myprofile/@profile/page.tsx
@@ -1,22 +1,36 @@
 'use client';
 
+import EditingIcon from '@/app/meeting/detail/components/EditingIcon';
 import ProfileIcon from '@/components/shared/BaseProfile';
+import { hover } from 'framer-motion';
 import { useState } from 'react';
 
 export default function Page() {
   const [nickname, setNickname] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [userBio, setUserBio] = useState('');
+  const [iconStatus, setIconStatus] = useState<'default' | 'hover' | 'editing'>('default');
 
   return (
-    <div className="w-full h-auto flex gap-6 items-center">
+    <div className="w-full h-auto flex gap-6 items-center relative">
+      <div
+        className="cursor-pointer absolute top-3 right-0"
+        onMouseEnter={() => setIconStatus('hover')}
+        onMouseLeave={() => setIconStatus('default')}
+      >
+        <EditingIcon status={iconStatus} />
+      </div>
       <ProfileIcon theme="light" size={102} />
       <div className="w-auto h-auto flex flex-col gap-[2px]">
         <div className="w-auto h-auto flex justify-start gap-2.5">
           <span className="text-black font-pretandard font-bold text-2xl">{nickname}</span>
-          <span className="text-black-6 font-pretandard font-medium text-lg">{userEmail}</span>
+          <span className="text-black-6 font-pretandard font-medium text-lg">
+            shy.penguin@codeit.com
+          </span>
         </div>
-        <div className="w-auto h-auto text-black-10 font-pretandard text-xl">{userBio}</div>
+        <div className="w-auto h-auto text-black-10 font-pretandard text-xl">
+          안녕하세요🙂 저는 조용하지만 할 말은 꼭 하는, 적당히 수줍은 펭귄입니다. 🐧
+        </div>
       </div>
     </div>
   );
