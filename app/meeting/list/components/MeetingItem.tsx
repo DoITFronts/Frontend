042366f09165e2239ff9e183c70355c5adb5e148
siggu <m0_w_0m@naver.com>
2025-03-10 10:02:49 +0900

    fix: hover 이벤트 삭제

--- app/meeting/list/components/MeetingItem.tsx
+++ app/meeting/list/components/MeetingItem.tsx
@@ -22,10 +22,11 @@ import Card from './Card';
 interface Props {
   meeting: Meeting;
   onClick: () => void;
+  onJoin: (meeting: Meeting) => Promise<boolean>;
   priority?: boolean;
 }
 
-export default function MeetingItem({ meeting, onClick, priority }: Props) {
+export default function MeetingItem({ meeting, onClick, onJoin, priority }: Props) {
   const { ref, inView } = useInView({ threshold: 0.1, triggerOnce: true });
   const { isLiked, handleLikeClick } = useLikeToggle(meeting.id, meeting.isLiked, onClick);
 
@@ -47,72 +48,70 @@ export default function MeetingItem({ meeting, onClick, priority }: Props) {
     >
       <Card>
         <div className="relative">
-          <div className="transition-shadow duration-300 hover:shadow-lg">
-            <Link href={`/meeting/detail/${meeting.id}`} className="block" prefetch={false}>
-              <div className="flex h-[430px] flex-col justify-between overflow-hidden">
-                {/* 이미지 */}
-                <div className="relative flex h-[200px] w-full items-center justify-center overflow-hidden">
-                  <Card.Like isLiked={isLiked} onClick={handleLikeClick} meetingId={meeting.id} />
-                  <div className="absolute left-0 top-0 z-0 size-[10px] bg-white" />
-                  <div className="absolute bottom-0 right-0 z-0 size-[10px] bg-white" />
-                  <Image
-                    src={meeting.imageUrl || '/assets/card/example_image.png'}
-                    width={384}
-                    height={200}
-                    alt="thumbnail"
-                    className="w-full"
-                    priority={priority}
-                  />
-                  <div className="absolute right-[14px] top-[17.5px]">
-                    <Category type={reverseCategoryMap[meeting.category]} />
-                  </div>
+          <Link href={`/meeting/detail/${meeting.id}`} className="block" prefetch={false}>
+            <div className="flex h-[430px] flex-col justify-between overflow-hidden">
+              {/* 이미지 */}
+              <div className="relative flex h-[200px] w-full items-center justify-center overflow-hidden">
+                <Card.Like isLiked={isLiked} onClick={handleLikeClick} meetingId={meeting.id} />
+                <div className="absolute left-0 top-0 z-0 size-[10px] bg-white" />
+                <div className="absolute bottom-0 right-0 z-0 size-[10px] bg-white" />
+                <Image
+                  src={meeting.imageUrl || '/assets/card/example_image.png'}
+                  width={384}
+                  height={200}
+                  alt="thumbnail"
+                  className="w-full"
+                  priority={priority}
+                />
+                <div className="absolute right-[14px] top-[17.5px]">
+                  <Category type={reverseCategoryMap[meeting.category]} />
                 </div>
+              </div>
 
-                {/* 상세 정보 */}
-                <div className="flex h-[206px] flex-col justify-between p-4 pt-0">
-                  <div className="flex flex-col gap-[10px]">
-                    <div className="flex flex-col gap-2">
-                      <Card.Title
-                        name={meeting.title}
-                        location={`${reverseCityMap[meeting.city] || meeting.city} ${reverseCityMap[meeting.town] || meeting.town}`}
-                      />
-                      {meeting.participants
-                        ?.filter((participant) => participant.isHost)
-                        .map((participant) => (
-                          <HostInfo
-                            key={participant.userId}
-                            name={participant.name}
-                            profileImage={participant.profileImage}
-                          />
-                        ))}
+              {/* 상세 정보 */}
+              <div className="flex h-[206px] flex-col justify-between p-4 pt-0">
+                <div className="flex flex-col gap-[10px]">
+                  <div className="flex flex-col gap-2">
+                    <Card.Title
+                      name={meeting.title}
+                      location={`${reverseCityMap[meeting.city] || meeting.city} ${reverseCityMap[meeting.town] || meeting.town}`}
+                    />
+                    {meeting.participants
+                      ?.filter((participant) => participant.isHost)
+                      .map((participant) => (
+                        <HostInfo
+                          key={participant.userId}
+                          name={participant.name}
+                          profileImage={participant.profileImage}
+                        />
+                      ))}
 
-                      <div className="flex h-[22px] flex-row items-center gap-1">
-                        <ChipInfo datetime={meeting.targetAt} />
-                      </div>
+                    <div className="flex h-[22px] flex-row items-center gap-1">
+                      <ChipInfo datetime={meeting.targetAt} />
                     </div>
-                    <div className="line-clamp-2 overflow-hidden text-ellipsis font-pretandard text-base font-medium text-[#8c8c8c]">
-                      {meeting.summary}
-                    </div>
+                  </div>
+                  <div className="line-clamp-2 overflow-hidden text-ellipsis font-pretandard text-base font-medium text-[#8c8c8c]">
+                    {meeting.summary}
                   </div>
                 </div>
               </div>
-            </Link>
-
-            {/* 하단 버튼 및 진행률 */}
-            <div className="flex flex-row items-center gap-4 p-4">
-              <MeetingProgress
-                id={meeting.id}
-                participantCount={meeting.participantCount}
-                capacity={meeting.capacity}
-                isConfirmed={meeting.isConfirmed}
-                isCompleted={meeting.isCompleted}
-              />
-              <Link href={`/meeting/detail/${meeting.id}`} passHref prefetch={false}>
-                <Button className="ml-6" disabled={meeting.isCompleted}>
-                  {meeting.isCompleted ? '마감' : '번개 참여'}
-                </Button>
-              </Link>
             </div>
+          </Link>
+
+          {/* 하단 버튼 및 진행률 */}
+          <div className="flex flex-row items-center gap-4 p-4">
+            <MeetingProgress
+              id={meeting.id}
+              participantCount={meeting.participantCount}
+              capacity={meeting.capacity}
+              isConfirmed={meeting.isConfirmed}
+              isCompleted={meeting.isCompleted}
+            />
+            <ButtonBox
+              chatIconDisabled
+              isJoined={meeting.isJoined}
+              onClick={() => onJoin(meeting)}
+            />
           </div>
         </div>
       </Card>
