db5ae3e1de82436b785334867072c5f577fb532a
Oh_hong <jungi9933@naver.com>
2025-03-11 14:36:52 +0900

    hotfix: kakaoMap 에러 수정

--- app/myprofile/layout.tsx
+++ app/myprofile/layout.tsx
@@ -20,7 +20,7 @@ export default function MyPageLayout({
 }) {
   console.log('meeting: ', meeting);
   return (
-    <div className="mx-auto max-w-[1200px] mt-[72px] flex flex-col gap-[38px]">
+    <div className="mx-auto max-w-[1200px] mt-[72px] mb-20 flex flex-col gap-[38px]">
       <div className="text-black font-dunggeunmo text-3xl">마이 페이지</div>
       <div className="w-full h-auto flex flex-col gap-10">
         <section>{profile}</section>
