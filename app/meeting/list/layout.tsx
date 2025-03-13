cec7f499671bbf540159e76fbfcd36eeb182c426
siggu <m0_w_0m@naver.com>
2025-03-13 18:28:05 +0900

    style: 모임 목록 반응형 UI 적용

--- app/meeting/list/layout.tsx
+++ app/meeting/list/layout.tsx
@@ -9,7 +9,7 @@ interface MeetingLayoutProps {
 
 export default function MeetingLayout({ title, meetingList }: MeetingLayoutProps) {
   return (
-    <div className="mx-auto flex max-w-5xl flex-col">
+    <div className="mx-auto mt-[30px] flex max-w-[1200px] flex-col px-4 md:mt-[72px] md:px-8 lg:w-[95%]">
       {title && <div>{title}</div>}
       {meetingList && <div>{meetingList}</div>}
     </div>
