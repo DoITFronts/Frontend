6cda3be6c951c728106cfb4c621f7ebc0546cdc9
soyoung <ssh0040233@gmail.com>
2025-03-14 15:55:44 +0900

    style: 랜딩페이지 px to rem

--- app/page.tsx
+++ app/page.tsx
@@ -30,20 +30,20 @@ export default function Page() {
         className="flex items-center justify-center"
       >
         <Lottie animationData={fireworkAnimation} loop />
-        <div className="flex flex-col items-center pt-[262px] sm:pt-[283px] md:pt-[282px]">
+        <div className="flex flex-col items-center pt-[16.375rem] sm:pt-[17.6875rem] md:pt-[17.625rem]">
           <Image
             src={Logo}
             className="md:w-[8.125rem] md:h-[1.7813rem] sm:w-[7.5rem] sm:h-[1.6438rem] w-[3.75rem] h-[0.825rem]"
             alt="번개팅 로고"
           />
-          <div className="mt-5 mb-[24.83px] sm:mt-10 sm:mb-[50px] md:mt-[56.43] md:mb-[81px] text-center text-3xl sm:text-6xl lg:text-7xl font-extrabold whitespace-nowrap">
+          <div className="mt-5 mb-[1.5519rem] sm:mt-10 sm:mb-[3.125rem] md:mt-[56.43] md:mb-[5.0625rem] text-center text-3xl sm:text-6xl lg:text-7xl font-extrabold whitespace-nowrap">
             <h1>즉흥적인 만남은</h1>
             <h1 className="text-yellow-6">여기서 시작!</h1>
           </div>
           <Button
             color="white"
             onClick={() => router.push('meeting/list')}
-            className="text-sm md:text-lg shadow-lg shadow-yellow-10 drop-shadow sm:w-[234px] sm:h-[60px] w-[100px] h-[40px]"
+            className="text-sm md:text-lg shadow-lg shadow-yellow-10 drop-shadow sm:w-[14.625rem] sm:h-[3.75rem] w-[6.25rem] h-[2.5rem]"
           >
             시작하기
           </Button>
@@ -51,16 +51,16 @@ export default function Page() {
         <Lottie animationData={fireworkAnimation} loop />
       </motion.section>
 
-      <section className="mt-[361px] sm:mt-[245.44px] md:mt-[350px] flex flex-col items-center gap-y-[394px] sm:gap-y-[400px] md:gap-y-[430px] px-[15px] sm:px-8">
+      <section className="mt-[22.5625rem] sm:mt-[15.34rem] md:mt-[21.875rem] flex flex-col items-center gap-y-[24.625rem] sm:gap-y-[25rem] md:gap-y-[26.875rem] px-[0.9375rem] sm:px-8">
         <motion.div
           initial="hidden"
           whileInView="visible"
           viewport={{ once: false }}
           variants={fadeInVariant}
-          className="flex sm:flex-row flex-col-reverse sm:items-center gap-10 sm:gap-4 md:gap-[78px] lg:w-[1200px] lg:px-[5%]"
+          className="flex sm:flex-row flex-col-reverse sm:items-center gap-10 sm:gap-4 md:gap-[4.875rem] lg:w-[75rem] lg:px-[5%]"
         >
           <div className="">
-            <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-5 md:mb-[30px]">
+            <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-5 md:mb-[1.875rem]">
               빠르게 약속을 잡고 싶다면?
               <br />
               번개팅으로.
@@ -75,7 +75,7 @@ export default function Page() {
           </div>
           <Image
             src={Section01}
-            className="w-[214px] h-[214px] sm:w-[40%] sm:h-[40%] md:w-[70%] md:h-[70%]"
+            className="w-[13.375rem] h-[13.375rem] sm:w-[40%] sm:h-[40%] md:w-[70%] md:h-[70%]"
             alt="section01"
           />
         </motion.div>
@@ -84,15 +84,15 @@ export default function Page() {
           whileInView="visible"
           viewport={{ once: false }}
           variants={fadeInVariant}
-          className="flex sm:flex-row flex-col sm:items-center items-end item text-right gap-10 sm:gap-4 md:gap-[78px] lg:w-[1200px] lg:px-[5%]"
+          className="flex sm:flex-row flex-col sm:items-center items-end item text-right gap-10 sm:gap-4 md:gap-[4.875rem] lg:w-[75rem] lg:px-[5%]"
         >
           <Image
             src={Section02}
-            className="w-[214px] h-[214px] sm:w-[40%] sm:h-[40%] md:w-[70%] md:h-[70%]"
+            className="w-[13.375rem] h-[13.375rem] sm:w-[40%] sm:h-[40%] md:w-[70%] md:h-[70%]"
             alt="section02"
           />
           <div className="">
-            <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-5 md:mb-[30px]">
+            <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-5 md:mb-[1.875rem]">
               모임 찾기부터 개설까지,
               <br />
               믿을 수 있는 번개⚡️
@@ -114,16 +114,16 @@ export default function Page() {
         whileInView="visible"
         viewport={{ once: false }}
         variants={fadeInVariant}
-        className="flex flex-col items-center justify-center pt-[513px] sm:pt-50 md:pt-[502px] pb-[299px] sm:pb-[285px] md:pb-[275px]"
+        className="flex flex-col items-center justify-center pt-[32.0625rem] sm:pt-50 md:pt-[31.375rem] pb-[18.6875rem] sm:pb-[17.8125rem] md:pb-[17.1875rem]"
       >
-        <h2 className=" pb-[50px] xl:pb-[83px] text-center text-xl sm:text-3xl md:text-4xl font-bold whitespace-nowrap">
+        <h2 className=" pb-[3.125rem] xl:pb-[5.1875rem] text-center text-xl sm:text-3xl md:text-4xl font-bold whitespace-nowrap">
           오늘, 새로운 사람들과 번개처럼 <br />
           빠르게 만나보세요! 🚀
         </h2>
         <Button
           color="white"
           onClick={() => router.push('meeting/list')}
-          className="sm:h-[60px] sm:w-[234px] h-[40px] w-[100px] text-sm sm:text-lg shadow-lg shadow-yellow-10 drop-shadow"
+          className="sm:h-[3.75rem] sm:w-[14.625rem] h-[2.5rem] w-[6.25rem] text-sm sm:text-lg shadow-lg shadow-yellow-10 drop-shadow"
         >
           시작하기
         </Button>
