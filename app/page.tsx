28aaed99577202720293fffcbdecf1cdcf367f39
chloe_choi <plumxeun@gmail.com>
2025-03-14 17:23:04 +0900

    DOIT-14 feat: conflict 해결

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import Button from '@/components/ui/button/Button';
+import PWAInstaller from '@/components/ui/PWAInstaller';
 import fireworkAnimation from '@/public/assets/landing/fireworks.json';
 import Section01 from '@/public/assets/landing/section01.svg';
 import Section02 from '@/public/assets/landing/section02.svg';
@@ -33,17 +34,17 @@ export default function Page() {
         <div className="flex flex-col items-center pt-[16.375rem] sm:pt-[17.6875rem] md:pt-[17.625rem]">
           <Image
             src={Logo}
-            className="md:w-[8.125rem] md:h-[1.7813rem] sm:w-[7.5rem] sm:h-[1.6438rem] w-[3.75rem] h-[0.825rem]"
+            className="h-[0.825rem] w-[3.75rem] sm:h-[1.6438rem] sm:w-[7.5rem] md:h-[1.7813rem] md:w-[8.125rem]"
             alt="번개팅 로고"
           />
-          <div className="mt-5 mb-[1.5519rem] sm:mt-10 sm:mb-[3.125rem] md:mt-[56.43] md:mb-[5.0625rem] text-center text-3xl sm:text-6xl lg:text-7xl font-extrabold whitespace-nowrap">
+          <div className="mb-[1.5519rem] mt-5 whitespace-nowrap text-center text-3xl font-extrabold sm:mb-[3.125rem] sm:mt-10 sm:text-6xl md:mb-[5.0625rem] md:mt-[56.43] lg:text-7xl">
             <h1>즉흥적인 만남은</h1>
             <h1 className="text-yellow-6">여기서 시작!</h1>
           </div>
           <Button
             color="white"
             onClick={() => router.push('meeting/list')}
-            className="text-sm md:text-lg shadow-lg shadow-yellow-10 drop-shadow sm:w-[14.625rem] sm:h-[3.75rem] w-[6.25rem] h-[2.5rem]"
+            className="h-10 w-[6.25rem] text-sm shadow-lg shadow-yellow-10 drop-shadow sm:h-[3.75rem] sm:w-[14.625rem] md:text-lg"
           >
             시작하기
           </Button>
@@ -51,21 +52,21 @@ export default function Page() {
         <Lottie animationData={fireworkAnimation} loop />
       </motion.section>
 
-      <section className="mt-[22.5625rem] sm:mt-[15.34rem] md:mt-[21.875rem] flex flex-col items-center gap-y-[24.625rem] sm:gap-y-[25rem] md:gap-y-[26.875rem] px-[0.9375rem] sm:px-8">
+      <section className="mt-[22.5625rem] flex flex-col items-center gap-y-[24.625rem] px-[0.9375rem] sm:mt-[15.34rem] sm:gap-y-[25rem] sm:px-8 md:mt-[21.875rem] md:gap-y-[26.875rem]">
         <motion.div
           initial="hidden"
           whileInView="visible"
           viewport={{ once: false }}
           variants={fadeInVariant}
-          className="flex sm:flex-row flex-col-reverse sm:items-center gap-10 sm:gap-4 md:gap-[4.875rem] lg:w-[75rem] lg:px-[5%]"
+          className="flex flex-col-reverse gap-10 sm:flex-row sm:items-center sm:gap-4 md:gap-[4.875rem] lg:w-[75rem] lg:px-[5%]"
         >
           <div className="">
-            <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-5 md:mb-[1.875rem]">
+            <h2 className="mb-4 text-xl font-bold sm:mb-5 sm:text-3xl md:mb-[1.875rem] md:text-4xl">
               빠르게 약속을 잡고 싶다면?
               <br />
               번개팅으로.
             </h2>
-            <p className="text-sm sm:text-xl font-extralight">
+            <p className="text-sm font-extralight sm:text-xl">
               바쁜 일상 속에서도 가볍게 즐길 수 있는 모임이 필요하신가요?
               <br />
               번개팅에서는 술자리, 보드게임, 카페, 맛집 등 다양한 번개 모임을 쉽게 탐색하고 즉시
@@ -75,7 +76,7 @@ export default function Page() {
           </div>
           <Image
             src={Section01}
-            className="w-[13.375rem] h-[13.375rem] sm:w-[40%] sm:h-[40%] md:w-[70%] md:h-[70%]"
+            className="size-[13.375rem] sm:size-2/5 md:size-[70%]"
             alt="section01"
           />
         </motion.div>
@@ -84,20 +85,20 @@ export default function Page() {
           whileInView="visible"
           viewport={{ once: false }}
           variants={fadeInVariant}
-          className="flex sm:flex-row flex-col sm:items-center items-end item text-right gap-10 sm:gap-4 md:gap-[4.875rem] lg:w-[75rem] lg:px-[5%]"
+          className="item flex flex-col items-end gap-10 text-right sm:flex-row sm:items-center sm:gap-4 md:gap-[4.875rem] lg:w-[75rem] lg:px-[5%]"
         >
           <Image
             src={Section02}
-            className="w-[13.375rem] h-[13.375rem] sm:w-[40%] sm:h-[40%] md:w-[70%] md:h-[70%]"
+            className="size-[13.375rem] sm:size-2/5 md:size-[70%]"
             alt="section02"
           />
           <div className="">
-            <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-5 md:mb-[1.875rem]">
+            <h2 className="mb-4 text-xl font-bold sm:mb-5 sm:text-3xl md:mb-[1.875rem] md:text-4xl">
               모임 찾기부터 개설까지,
               <br />
               믿을 수 있는 번개⚡️
             </h2>
-            <p className="text-sm sm:text-xl font-extralight">
+            <p className="text-sm font-extralight sm:text-xl">
               번개팅에서는 단순히 모임을 찾는 것뿐만 아니라, 실제 참여자들의 리뷰를 확인하고 소통할
               수 있어요. 참여 전에 유저들의 생생한 후기를 살펴보고, 궁금한 점이 있다면 채팅으로 직접
               질문해보세요! 주최자와 참가자 간의 자유로운 소통을 통해 모임 분위기, 일정, 준비
