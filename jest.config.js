c6bf7679841fe39d53df2ce880d796cdf6fe95b7
soyoung <ssh0040233@gmail.com>
2025-03-16 19:57:47 +0900

    chore: jest test setting

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+.(ts|tsx|js|jsx)$": "babel-jest",
+  },
 };
