d7aba86d3fa1b1f35c9c0c9a8aa27efba52015f8
siggu <siggu100@gmail.com>
2025-03-17 22:48:52 +0900

    fix: BaseProfile 오류 해결

--- app/meeting/list/components/MeetingList.tsx
+++ app/meeting/list/components/MeetingList.tsx
@@ -1,30 +1,30 @@
-'use client';
+"use client";
 
-import { ko } from 'date-fns/locale';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { useState, useEffect, useMemo, useRef } from 'react';
-import DatePicker from 'react-datepicker';
+import { ko } from "date-fns/locale";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState, useEffect, useMemo, useRef } from "react";
+import DatePicker from "react-datepicker";
 
-import CardItem from '@/components/ui/card/CardItem';
-import CategoryFilter from '@/components/ui/chip/CategoryFilter';
-import DropDown from '@/components/ui/dropdown/DropDown';
-import FilterDropdown from '@/components/ui/dropdown/FilterDropdown';
-import EmptyMessage from '@/components/ui/list/EmptyMessage';
-import Icon from '@/components/utils/Icon';
-import useMeetingList from '@/hooks/meeting/useMeetingList';
+import CardItem from "@/components/ui/card/CardItem";
+import CategoryFilter from "@/components/ui/chip/CategoryFilter";
+import DropDown from "@/components/ui/dropdown/DropDown";
+import FilterDropdown from "@/components/ui/dropdown/FilterDropdown";
+import EmptyMessage from "@/components/ui/list/EmptyMessage";
+import Icon from "@/components/utils/Icon";
+import useMeetingList from "@/hooks/meeting/useMeetingList";
 import {
   defaultFilter,
   defaultFirstOption,
   defaultSecondOption,
   participantFilter,
-} from '@/lib/constants/meeting';
-import meetingCategory from '@/lib/constants/meeting/meeting';
-import modalStore from '@/store/modalStore';
-import { Meeting } from '@/types/meeting/meeting';
-import { regions } from '@/types/map/regions';
-import { formatShortDate } from '@/utils/timeUtils/formatDateTime';
+} from "@/lib/constants/meeting";
+import meetingCategory from "@/lib/constants/meeting/meeting";
+import modalStore from "@/store/modalStore";
+import { Meeting } from "@/types/meeting/meeting";
+import { regions } from "@/types/map/regions";
+import { formatShortDate } from "@/utils/timeUtils/formatDateTime";
 
-import { MeetingCardLoading } from './skeleton/MeetingCardSkeleton';
+import { MeetingCardLoading } from "./skeleton/MeetingCardSkeleton";
 
 interface InitialMeetingsProps {
   initialMeetings: Meeting[];
@@ -35,17 +35,23 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
   const router = useRouter();
 
   // URL에서 가져온 검색 조건을 상태로 관리
-  const [selectedCategory, setSelectedCategory] = useState(searchParams.get('category') || '전체');
+  const [selectedCategory, setSelectedCategory] = useState(
+    searchParams.get("category") || "전체",
+  );
   const [selectedFirstLocation, setSelectedFirstLocation] = useState(
-    searchParams.get('location_1') || defaultFirstOption,
+    searchParams.get("location_1") || defaultFirstOption,
   );
   const [selectedSecondLocation, setSelectedSecondLocation] = useState(
-    searchParams.get('location_2') || defaultSecondOption,
+    searchParams.get("location_2") || defaultSecondOption,
   );
   const [selectedDate, setSelectedDate] = useState(
-    searchParams.get('targetAt') ? new Date(searchParams.get('targetAt') as string) : null,
+    searchParams.get("targetAt")
+      ? new Date(searchParams.get("targetAt") as string)
+      : null,
   );
-  const [selectedFilter, setSelectedFilter] = useState(searchParams.get('order') || '');
+  const [selectedFilter, setSelectedFilter] = useState(
+    searchParams.get("order") || "",
+  );
   const observerRef = useRef<HTMLDivElement | null>(null);
   const { openModal } = modalStore();
 
@@ -53,27 +59,33 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
   const [tempDate, setTempDate] = useState<Date | null>(selectedDate);
 
   useEffect(() => {
-    setSelectedCategory(searchParams.get('category') || '전체');
-    setSelectedFirstLocation(searchParams.get('location_1') || defaultFirstOption);
-    setSelectedSecondLocation(searchParams.get('location_2') || defaultSecondOption);
-    setSelectedFilter(searchParams.get('order') || '');
+    setSelectedCategory(searchParams.get("category") || "전체");
+    setSelectedFirstLocation(
+      searchParams.get("location_1") || defaultFirstOption,
+    );
+    setSelectedSecondLocation(
+      searchParams.get("location_2") || defaultSecondOption,
+    );
+    setSelectedFilter(searchParams.get("order") || "");
   }, [searchParams]);
 
   // URL을 변경하여 상태 업데이트
   const updateSearchParams = (key: string, value: string) => {
     const params = new URLSearchParams(searchParams.toString());
     if (value) {
-      params.set(key === 'date' ? 'targetAt' : key, value);
+      params.set(key === "date" ? "targetAt" : key, value);
     } else {
-      params.delete(key === 'date' ? 'targetAt' : key);
+      params.delete(key === "date" ? "targetAt" : key);
     }
-    router.replace(`?${decodeURIComponent(params.toString())}`, { scroll: false });
+    router.replace(`?${decodeURIComponent(params.toString())}`, {
+      scroll: false,
+    });
   };
 
   //  카테고리 변경 핸들러
   const handleCategoryClick = (category: string) => {
     setSelectedCategory(category);
-    updateSearchParams('category', category === '전체' ? '' : category);
+    updateSearchParams("category", category === "전체" ? "" : category);
   };
 
   // 첫 번째 지역 선택
@@ -82,15 +94,15 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
 
     // 첫 번째 지역을 업데이트
     if (selected === defaultFirstOption) {
-      params.delete('location_1');
-      params.delete('location_2'); // 첫 번째 지역을 초기화하면 두 번째 지역도 초기화
+      params.delete("location_1");
+      params.delete("location_2"); // 첫 번째 지역을 초기화하면 두 번째 지역도 초기화
     } else {
-      params.set('location_1', selected);
+      params.set("location_1", selected);
 
       // 현재 선택된 두 번째 지역이 유효한지 확인 후 유지
       const validSecondLocations = regions[selected] || [];
       if (!validSecondLocations.includes(selectedSecondLocation)) {
-        params.delete('location_2');
+        params.delete("location_2");
       }
     }
 
@@ -100,7 +112,7 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
   // 두 번째 지역 선택
   const handleSelectSecondLocation = (selected: string) => {
     setSelectedSecondLocation(selected);
-    updateSearchParams('location_2', selected);
+    updateSearchParams("location_2", selected);
   };
 
   // 날짜 확인 핸들러
@@ -109,27 +121,36 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
     if (tempDate) {
       const fixedDate = new Date(tempDate);
       fixedDate.setHours(12, 0, 0, 0); // UTC 보정
-      updateSearchParams('targetAt', `${fixedDate.toISOString().split('T')[0]}T00:00:00`);
+      updateSearchParams(
+        "targetAt",
+        `${fixedDate.toISOString().split("T")[0]}T00:00:00`,
+      );
     }
   };
 
   // 마감 임박, 참여 인원 필터링 클릭 핸들러
   const handleSelectFilter = (selected: string) => {
     setSelectedFilter(selected);
-    updateSearchParams('order', selected);
+    updateSearchParams("order", selected);
   };
 
   // useInfiniteQuery를 사용해 번개 데이터 가져오기
-  const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useMeetingList({
-      category: selectedCategory,
-      city: selectedFirstLocation,
-      town: selectedSecondLocation,
-      targetAt: selectedDate,
-      size: 10,
-      initialMeetings,
-      order: selectedFilter,
-    });
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useMeetingList({
+    category: selectedCategory,
+    city: selectedFirstLocation,
+    town: selectedSecondLocation,
+    targetAt: selectedDate,
+    size: 10,
+    initialMeetings,
+    order: selectedFilter,
+  });
 
   // 번개 데이터 통합
   const meetings = useMemo(
@@ -148,7 +169,7 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
       (entries) => {
         if (entries[0].isIntersecting) fetchNextPage();
       },
-      { rootMargin: '150px', threshold: 0.3 },
+      { rootMargin: "150px", threshold: 0.3 },
     );
 
     observer.observe(observerRef.current);
@@ -157,7 +178,10 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
   }, [fetchNextPage, hasNextPage]);
 
   // 지역 목록 생성
-  const meetingLocationFirst = useMemo(() => [defaultFirstOption, ...Object.keys(regions)], []);
+  const meetingLocationFirst = useMemo(
+    () => [defaultFirstOption, ...Object.keys(regions)],
+    [],
+  );
   const meetingLocationSecond = useMemo(
     () => [defaultSecondOption, ...(regions[selectedFirstLocation] || [])],
     [selectedFirstLocation],
@@ -167,13 +191,13 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
   const handleResetDate = () => {
     setSelectedDate(null);
     setTempDate(null);
-    updateSearchParams('targetAt', '');
+    updateSearchParams("targetAt", "");
   };
 
   // 마감 임박, 참여 인원 필터링 초기화 클릭 핸들러
   const handleResetFilter = () => {
-    setSelectedFilter('');
-    updateSearchParams('order', '');
+    setSelectedFilter("");
+    updateSearchParams("order", "");
   };
 
   // 캘린더 내부 스타일
@@ -186,17 +210,19 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
     const isFiltered = selectedDate?.getTime() === date.getTime();
 
     return {
-      width: '32px',
-      height: '32px',
-      display: 'flex',
-      padding: '10px',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: '5px',
-      color: '#8c8c8c',
+      width: "32px",
+      height: "32px",
+      display: "flex",
+      padding: "10px",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "5px",
+      color: "#8c8c8c",
 
-      ...(isToday && { fontWeight: 'bold', color: 'black' }),
-      ...(isSelected || isFiltered ? { backgroundColor: 'black', color: 'white' } : {}),
+      ...(isToday && { fontWeight: "bold", color: "black" }),
+      ...(isSelected || isFiltered
+        ? { backgroundColor: "black", color: "white" }
+        : {}),
     };
   };
 
@@ -214,7 +240,7 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
             <CategoryFilter
               text={category}
               size="lg"
-              mode={selectedCategory === category ? 'dark' : 'light'}
+              mode={selectedCategory === category ? "dark" : "light"}
             />
           </button>
         ))}
@@ -244,13 +270,15 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
                   onChange={setTempDate}
                   minDate={new Date()}
                   calendarClassName="custom-calendar"
-                  renderDayContents={(day, date) => <div style={getDayStyle(date)}>{day}</div>}
+                  renderDayContents={(day, date) => (
+                    <div style={getDayStyle(date)}>{day}</div>
+                  )}
                 />
                 <div className="flex justify-between">
                   <button
                     type="button"
                     onClick={handleResetDate}
-                    className="inline-flex items-center justify-center gap-2.5 self-stretch overflow-hidden rounded-xl border border-[#1e1e1e] bg-white  py-2.5"
+                    className="inline-flex items-center justify-center gap-2.5 self-stretch overflow-hidden rounded-xl border border-[#1e1e1e] bg-white py-2.5"
                   >
                     <div className="relative w-[145px] justify-start text-center font-['Pretendard'] text-sm font-semibold leading-tight text-[#1e1e1e]">
                       초기화
@@ -269,14 +297,16 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
               </div>
             }
             trigger={
-              <div className="inline-flex h-9 flex-row items-center justify-center rounded-xl border border-[#8c8c8c] bg-white px-2.5 py-2 text-center font-pretandard text-sm font-medium leading-tight text-[#8c8c8c] hover:bg-[#595959] hover:text-white md:h-10">
-                {selectedDate ? formatShortDate(selectedDate.toISOString()) : '날짜'}
+              <div className="font-pretandard inline-flex h-9 flex-row items-center justify-center rounded-xl border border-[#8c8c8c] bg-white px-2.5 py-2 text-center text-sm font-medium leading-tight text-[#8c8c8c] hover:bg-[#595959] hover:text-white md:h-10">
+                {selectedDate
+                  ? formatShortDate(selectedDate.toISOString())
+                  : "날짜"}
                 <div onClick={handleResetDate}>
-                  <Icon path={selectedDate ? 'exit' : 'chevron_down'} />
+                  <Icon path={selectedDate ? "exit" : "chevron_down"} />
                 </div>
               </div>
             }
-            onSelect={() => openModal('calendar')}
+            onSelect={() => openModal("calendar")}
           />
         </div>
         <DropDown
@@ -285,13 +315,17 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
           selectedValue={selectedFilter}
           onSelect={handleSelectFilter}
           trigger={
-            <div className="inline-flex h-9 flex-row items-center justify-center rounded-xl border border-[#8c8c8c] bg-white px-2.5 py-2 text-center font-pretandard text-sm font-medium leading-tight text-[#8c8c8c] hover:bg-[#595959] hover:text-white md:h-10">
-              <div onClick={handleResetFilter} aria-label="필터 초기화" className="cursor-pointer">
-                <Icon path={selectedFilter ? 'exit' : 'sort'} />
+            <div className="font-pretandard inline-flex h-9 flex-row items-center justify-center rounded-xl border border-[#8c8c8c] bg-white px-2.5 py-2 text-center text-sm font-medium leading-tight text-[#8c8c8c] hover:bg-[#595959] hover:text-white md:h-10">
+              <div
+                onClick={handleResetFilter}
+                aria-label="필터 초기화"
+                className="cursor-pointer"
+              >
+                <Icon path={selectedFilter ? "exit" : "sort"} />
               </div>
             </div>
           }
-          optionClassName="justify-start min-w-[95px] py-[10px] px-4 text-[#8c8c8c] text-base font-semibold font-pretandard leading-normal"
+          optionClassName="justify-start min-w-[115px] py-[10px] px-4 text-[#8c8c8c] text-base font-semibold font-pretandard leading-normal"
         />
       </div>
 
@@ -305,7 +339,10 @@ export default function MeetingList({ initialMeetings }: InitialMeetingsProps) {
           </div>
         )}
         {!isLoading && !isError && meetings.length === 0 && (
-          <EmptyMessage firstLine="아직 번개가 없어요" secondLine="지금 번개를 만들어 보세요!" />
+          <EmptyMessage
+            firstLine="아직 번개가 없어요"
+            secondLine="지금 번개를 만들어 보세요!"
+          />
         )}
         {!isLoading && !isError && (
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
