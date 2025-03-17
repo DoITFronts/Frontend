aaf37f020bd3ac741365386bd0e084c531324820
SoYoung Oh <111185468+Ocean423@users.noreply.github.com>
2025-03-17 21:16:34 +0900

    !HOFIX: guest user fix

--- app/user/signin/page.tsx
+++ app/user/signin/page.tsx
@@ -21,7 +21,7 @@ export default function Signin() {
   // 테스트 계정 버튼 handler
   const handleTestAccountClick = () => {
     // 테스트 계정 자동 입력 처리
-    setValue('username', 'asdf@gmail.com');
+    setValue('username', 'guest@gmail.com');
     setValue('password', 'asdf1234!!');
 
     // 자동 로그인
