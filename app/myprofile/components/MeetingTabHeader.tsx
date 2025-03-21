851344ac422a5a11762c49e0da3e5b8089da75fd
Oh_hong <jungi9933@naver.com>
2025-03-21 14:24:18 +0900

    refactor: MeetingTabHeader 컴포넌트 분리

---
+++ app/myprofile/components/MeetingTabHeader.tsx
@@ -0,0 +1,31 @@
+import CategoryFilter from "@/components/ui/chip/CategoryFilter";
+
+interface MeetingTabHeaderProps {
+  selectedMenuTab: string;
+  handleMenuClick: (tab: string) => void;
+  menuTabs: string[];
+}
+
+export default function MeetingTabHeader({
+  selectedMenuTab,
+  handleMenuClick,
+  menuTabs,
+}: MeetingTabHeaderProps) {
+  return (
+    <div className="flex size-auto items-center gap-2.5 md:gap-3 lg:gap-3">
+      {menuTabs.map((tab) => (
+        <button
+          className="cursor-pointer"
+          key={tab}
+          onClick={() => handleMenuClick(tab)}
+        >
+          <CategoryFilter
+            text={tab}
+            size="lg"
+            mode={tab === selectedMenuTab ? "dark" : "light"}
+          />
+        </button>
+      ))}
+    </div>
+  );
+}
