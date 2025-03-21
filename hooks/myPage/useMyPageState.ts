1cacb806326639b16a3cb289daeb27e9987b90c2
Oh_hong <jungi9933@naver.com>
2025-03-21 14:14:28 +0900

    refactor: useMyPageState hook을 통한 상태 로직 분리

---
+++ hooks/myPage/useMyPageState.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useMyPageState() {
+  const [selectedMenuTab, setSelectedMenuTab] = useState("나의 번개");
+  const [selectedActivityTab, setSelectedActivityTab] = useState("");
+
+  const handleMenuClick = (tab: string) => {
+    if (tab === selectedMenuTab) {
+      setSelectedMenuTab("");
+    } else {
+      setSelectedMenuTab(tab);
+    }
+  };
+
+  const handleActiviryClick = (tab: string) => {
+    if (tab === selectedActivityTab) {
+      setSelectedActivityTab("");
+    } else {
+      setSelectedActivityTab(tab);
+    }
+  };
+
+  return {
+    selectedMenuTab,
+    selectedActivityTab,
+    handleMenuClick,
+    handleActiviryClick,
+  };
+}
